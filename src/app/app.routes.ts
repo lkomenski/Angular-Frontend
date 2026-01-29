dd7ac74ebd204666f0af65db49b69a9455ba0674
Leena Komenski <lkomenski@gmail.com>
2026-01-29 14:17:49 -0800

    refactor: reorganize route definitions and remove redundant paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../pages/home/home.component';
-import { ListComponent } from '../pages/list/list.component';
-import { DetailComponent } from '../pages/detail/detail.component';
 import { AdminComponent } from '../pages/admin/admin.component';
+import { ListComponent } from '../pages/list/list.component';
 import { CreateEditComponent } from '../pages/edit/edit.component';
+import { DetailComponent } from '../pages/detail/detail.component';
 import { authGuard } from '../guards/auth.guard';
 
-export const routes: Routes = [
+export const routes: Routes = [ 
   { path: '', component: HomeComponent },
+  { path: 'admin', component: AdminComponent },
   { path: 'list', component: ListComponent },
-  { path: 'items', component: ListComponent },
-  { path: 'items/:id', component: DetailComponent },
-  { path: 'detail/:id', component: DetailComponent },
-  { path: 'admin', component: AdminComponent, canActivate: [authGuard] },
   { path: 'create', component: CreateEditComponent, canActivate: [authGuard] },
   { path: 'edit/:id', component: CreateEditComponent, canActivate: [authGuard] },
+  { path: 'detail/:id', component: DetailComponent },
   { path: '**', redirectTo: '' },
 ];
