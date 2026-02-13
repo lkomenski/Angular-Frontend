8121ba4d9ed4cc670ffa1b63edda9ed7bbe66632
Leena Komenski <lkomenski@gmail.com>
2026-02-12 16:43:37 -0800

    refactor: update comments in UserResponse type for clarity and maintainability

--- src/models/models.ts
+++ src/models/models.ts
@@ -12,6 +12,9 @@ export type UserResponse =
 
 /*
 I used an interface for User because interfaces are ideal for defining object shapes
-and can be easily extended or implemented by classes in the future. Types were chosen
-for UserStatus and UserResponse because they better represent union types and literal values.
+and can be easily extended or implemented by classes in the future. User is an object with properties
+and therefore was better suited for an interface. Types were chosen for UserStatus and 
+UserResponse because they better represent union types and literal values. Both of these
+will probably not require extension or more varied implementation, so types make more sense for them. 
+Types also allow for more complex type definitions, which is useful for UserResponse.
  */
